4bb7f8c5151cd32269c3e904825117c04134e619
RavdeepCts <ravdeep.singh@cognizant.com>
2024-04-15 16:33:23 +0530

    Home,Reservation module created and basic form created

---
+++ src/app/reservation-form/reservation-form.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-reservation-form',
+  templateUrl: './reservation-form.component.html',
+  styleUrls: ['./reservation-form.component.css']
+})
+export class ReservationFormComponent implements OnInit {
+  reservationForm : FormGroup = new FormGroup({});
+  constructor() { }
+
+  onSubmit(){
+    
+  }
+  ngOnInit(): void {
+  }
+
+}
